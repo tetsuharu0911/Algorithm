1acb27c9857638270c45ca3ed5d9f5d31d75414b
tetsuharu0911 <tetsuharu.nasu@gmail.com>
2023-09-18 16:40:11 +0900

    修正

--- bfs/bfs.ts
+++ bfs/bfs.ts
@@ -1,40 +1,42 @@
+import { Queue } from './Queue';
+
 type Graph = {[key: string]: string[]};
 
 const bfs = (graph: Graph, start: string) => {
-    let visited = new Set<string>();
-    let queue = new Queue<string>();
+  let visited = new Set<string>();
+  let queue = new Queue<string>();
 
-    visited.add(start);
-    queue.enqueue(start);
+  visited.add(start);
+  queue.enqueue(start);
 
-    while(!queue.isEmpty()) {
-        let currentNode = queue.dequeue();
+  while (!queue.isEmpty()) {
+    let currentNode = queue.dequeue();
 
-        if (currentNode === undefined) {
-            continue;
-        }
-
-        console.log('Visited: ${currentNode}');
-
-        let neighbors = graph[currentNode];
-
-        for (let neighbor of neighbors) {
-            if (!visited.has(neighbor)) {
-                visited.add(neighbor);
-                queue.enqueue(neighbor);
-            }
-        }
+    if (currentNode === undefined) {
+      continue;
     }
+
+    console.log(`Visited: ${currentNode}`);
+
+    let neighbors = graph[currentNode];
+
+    for (let neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.enqueue(neighbor);
+      }
+    }
+  }
 };
 
 // グラフの定義
 const graph: Graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
+  'A': ['B', 'C'],
+  'B': ['D', 'E'],
+  'C': ['F'],
+  'D': [],
+  'E': ['F'],
+  'F': []
 };
 
 // アルゴリズムの実行
